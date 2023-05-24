164f8b4242aa5dece0f823c0d7b25ecd64a4e9c6
djamiranda <djamiranda@gmail.com>
2023-05-24 13:50:20 -0400

    added APIKey & URL

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    var APIKEY = "8c7df6150f9dfe4b9fc100e2519e4fd4"
+    var baseURL = "https://api.openweathermap.org/data/2.5/forecast?";
+    var cityData = []
+}
+)
